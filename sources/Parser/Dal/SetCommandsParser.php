873c007e6ec686da94680540e8ac105e40837ef3
paranoiq <cz.paranoiq@gmail.com>
2023-02-18 19:42:19 +0100

    BCBreak: fixed := not being supported in all places. Classes ..\Dal\Set\SetAssignment and ..\Sql\Dml\Assignement merged into ..\Sql\Assignment. :=  is never normalized to =

--- sources/Parser/Dal/SetCommandsParser.php
+++ sources/Parser/Dal/SetCommandsParser.php
@@ -15,8 +15,8 @@ use SqlFtw\Parser\ExpressionParser;
 use SqlFtw\Parser\ParserException;
 use SqlFtw\Parser\TokenList;
 use SqlFtw\Parser\TokenType;
+use SqlFtw\Sql\Assignment;
 use SqlFtw\Sql\Dal\Set\ResetPersistCommand;
-use SqlFtw\Sql\Dal\Set\SetAssignment;
 use SqlFtw\Sql\Dal\Set\SetCharacterSetCommand;
 use SqlFtw\Sql\Dal\Set\SetNamesCommand;
 use SqlFtw\Sql\Dal\Set\SetVariablesCommand;
@@ -137,7 +137,7 @@ class SetCommandsParser
     }
 
     /**
-     * @return non-empty-list<SetAssignment>
+     * @return non-empty-list<Assignment>
      */
     private function parseAssignments(TokenList $tokenList): array
     {
@@ -217,7 +217,7 @@ class SetCommandsParser
                 }
             }
 
-            $assignments[] = new SetAssignment($variable, $expression, $operator);
+            $assignments[] = new Assignment($variable, $expression, $operator);
             $current = false;
         } while ($tokenList->hasSymbol(','));
 
